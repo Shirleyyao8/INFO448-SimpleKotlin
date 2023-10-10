cc39866b8a0c77600a22ffb96bad80b3ccd908da
Yuhui Shirley Yao <yuhui@Yuhuis-MacBook-Air.local>
2023-10-10 11:33:53 -0700

    completed

--- src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
+++ src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
@@ -18,39 +18,38 @@ class Library {
 // If it is any other number, return "a number"
 // Otherwise, return "I don't understand"
 fun whenFn(value: Any): String {
-    if (Any is String) {
-        if (Any.equals("Hello")) {   
-            return "world";
-        } else if (Any.equals("Howdy") || Any.equals("Bonjour")) {
+    if (value is String) {
+        if (value == "Hello") {   
+            return "world"
+        } else if (value == "Howdy" || value == "Bonjour") {
             return "Say what?"
         }
-    } else if (Any is Number) {
-        if (Any == 0) {
+    } else if (value is Number) {
+        if (value == 0) {
             return "zero";
-        } else if (Any==1) {
-            return "one";
-        } else if (Any<=10 && Any>==2) {
-            return "low number";
+        } else if (value == 1) {
+            return "one"
+        } else if (value in 2..10) {
+            return "low number"
         } 
-    } else {
-        return "I don't understand"; // was thinking this is the only needed one
-    }
+    } 
+        return "I don't understand"
 } 
 
 
 // write an "add" function that takes two Ints, returns an Int, and adds the values
 fun add(a: Int, b: Int): Int {
-    return a + b
+    return a + b;
 }
 
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
-fun sub(a: Int, b: Int) Int {
-    return a - b
+fun sub(a: Int, b: Int): Int {
+    return a - b;
 }
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
 fun mathOp(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
     // Call the passed-in function with the two integer arguments
-    return operation(a, b)
+    return operation(a, b);
 }
 
 // typealias Function = (Int, Int) -> Int
@@ -62,7 +61,7 @@ fun mathOp(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
 // write a class "Person" with first name, last name and age
 class Person(var firstName: String, val lastName: String, var age: Int) {
     val debugString: String
-        get() = "Person firstName:${firstName} lastName:${lastName} age:${age}"
+        get() = "[Person firstName:${firstName} lastName:${lastName} age:${age}]"
 
     // init {
     //     return "Person firstName:${firstName} lastName:${lastName} age:${age}"
@@ -70,51 +69,40 @@ class Person(var firstName: String, val lastName: String, var age: Int) {
 }
 
 // write a class "Money" with amount and currency, and define a convert() method and the "+" operator
-class Money(var amount: Int, var currency: String) {
-    var amount: Int
-    get() = amount
-    set(value) {
-        if(value >= 0) {
-            amount = value
-        } else {
+class Money(val amount: Int, val currency: String) {
+    init {
+        if (amount < 0) {
             throw IllegalArgumentException("Amount cannot be less than zero")
         }
-    }
-
-    var currency: String
-    get() = currency
-    set(value) {
-        if(value in setOf(""USD", "EUR", "CAN", "GBP"")) {
-            currency = value
-        } else {
+        if (currency !in setOf("USD", "EUR", "CAN", "GBP")) {
             throw IllegalArgumentException("Invalid currency")
         }
-    } 
+    }
 
     fun convert(targetCurrency: String): Money {
         val conversionRate= when (currency) {
             "USD" -> when (targetCurrency) {
                 "GBP" -> 0.5
                 "EUR" -> 1.5
-                "CAN" -> 0.8
+                "CAN" -> 1.25
                 else -> 1.0
             }
             "GBP" -> when (targetCurrency) {
                 "USD" -> 2.0
-                "EUR" -> 1.5
+                "EUR" -> 3.0 
                 "CAN" -> 1.6 
                 else -> 1.0
             }
             "EUR" -> when (targetCurrency) {
-                "USD" -> 1.33
-                "GBP" -> 0.67
-                "CAN" -> 1.07
+                "USD" -> 2.0 / 3.0
+                "GBP" -> 1.0 / 3.0
+                "CAN" -> 2.5 / 3.0
                 else -> 1.0
             }
             "CAN" -> when (targetCurrency) {
-                "USD" -> 1.25
-                "GBP" -> 0.625
-                "EUR" -> 0.935
+                "USD" -> 0.8
+                "GBP" -> 5.0 / 8.0
+                "EUR" -> 3.0 / 2.5
                 else -> 1.0
             }
             // or say else -> 1.0 ??
@@ -124,14 +112,16 @@ class Money(var amount: Int, var currency: String) {
         return Money(convertedAmount, targetCurrency)
     }
 
-    operator fun plus(other: Money): Money {  
-     // if the two currency are the same
-    val convertedAmount = if (currency = other.currency) {
-        this.amount + other.amount
-    } else {
-    // if the two currency are not the same
-        other.convert(currency) + this.amount
+    operator fun plus(other: Money): Money {
+        return if (currency == other.currency) {
+            Money(amount + other.amount, currency)
+        } else {
+            val convertedOther = other.convert(currency)
+            Money(amount + convertedOther.amount, currency)
+        }
     }
-       return Money(convertedAmount, currency)
+
+    override fun toString(): String {
+        return "$amount $currency"
     }
 }
